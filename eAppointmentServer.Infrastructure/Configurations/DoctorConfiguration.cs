d88566f4196ca0b9944c9b7582ea0f64c55aa4ef
Berken Yünlü <berkenyunlu@gmail.com>
2024-04-13 23:20:33 +0300

    DbConfiguration , Repositories , Json Web Token Configuration , Scrutor

---
+++ eAppointmentServer.Infrastructure/Configurations/DoctorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using eAppointmentServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using eAppointmentServer.Domain.Enums;
+
+namespace eAppointmentServer.Infrastructure.Configurations;
+
+internal sealed class DoctorConfiguration : IEntityTypeConfiguration<Doctor>
+{
+    public void Configure(EntityTypeBuilder<Doctor> builder)
+    {
+        builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
+        builder.Property(p => p.LastName).HasColumnType("varchar(50)");
+
+        builder.Property(p => p.Department)
+            .HasConversion(v => v.Value, v => DepartmentEnum.FromValue(v))
+            .HasColumnName("Department");
+    }
+}
---
+++ eAppointmentServer.Infrastructure/Services/JwtProvider.cs
@@ -0,0 +1,35 @@
+﻿using eAppointmentServer.Application.Services;
+using eAppointmentServer.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace eAppointmentServer.Infrastructure.Services;
+
+internal sealed class JwtProvider : IJwtProvider
+{
+    public string CreateToken(AppUser user)
+    {
+        List<Claim> claims = new List<Claim>()
+        {
+            new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+            new Claim(ClaimTypes.Name,user.FullName),
+            new Claim(ClaimTypes.Email,user.Email??string.Empty),
+            new Claim("UserName",user.UserName??string.Empty),
+        };
+        DateTime expires = DateTime.Now.AddDays(1);
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(string.Join("-", Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid())));
+        SigningCredentials signingCredentials = new(securityKey,SecurityAlgorithms.HmacSha512);
+        JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(
+            issuer: "Berken Azad Yünlü",
+            audience: "eAppointment",
+            claims: claims,
+            notBefore: DateTime.Now,
+            expires: expires,
+            signingCredentials: signingCredentials);
+        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+        string token = handler.WriteToken(jwtSecurityToken);
+        return token;
+    }
+}
